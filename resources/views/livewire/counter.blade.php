a07d29e20e5c4135c901ddf82ffa74ff9cca8b1e
sk-sajib <sksajib0000@gmail.com>
2023-01-05 09:22:09 +0600

    Part #1: ===== - Databse Design - Database table creation
    Part #2:
    ======
    - Database seeding
    - Database basic user role creation and assign with user model
    
    Part #3:
    ======
    - LiveWire install and configure
    - LiveWire Component creation and use

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <p>{{ $count }}</p>
+    <button class="bg-slate-700 px-4 py-2 text-white" wire:click="increase('Thanks for choosing us')">Enter</button>
+    <button class="bg-slate-700 px-4 py-2 text-white" wire:click="decrease('why leaving us??')">Quit</button>
+</div>
